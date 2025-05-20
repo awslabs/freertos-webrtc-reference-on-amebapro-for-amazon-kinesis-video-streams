9109e7cba680f98ea05f9a1bed09669561e771e7
ActoryOu <ousc@amazon.com>
2025-05-20 02:12:59 +0000

    libsrtp

--- CMake/libsrtp.cmake
+++ CMake/libsrtp.cmake
@@ -1,39 +1,46 @@
 cmake_minimum_required(VERSION 3.6)
+include( ${REPO_ROOT_DIRECTORY}/project/realtek_amebapro2_webrtc_application/GCC-RELEASE/includepath.cmake )
 
 project(libsrtp)
 set(libsrtp libsrtp)
 
 # libsrtp library source files.
 file(GLOB LIBSRTP_GLOB_SOURCES
-          ${CMAKE_CURRENT_LIST_DIR}/../libraries/libsrtp/crypto/kernel/*.c
-          ${CMAKE_CURRENT_LIST_DIR}/../libraries/libsrtp/crypto/math/*.c
-          ${CMAKE_CURRENT_LIST_DIR}/../libraries/libsrtp/crypto/replay/*.c
-          ${CMAKE_CURRENT_LIST_DIR}/../libraries/libsrtp/srtp/*.c )
+          ${REPO_ROOT_DIRECTORY}/libraries/libsrtp/crypto/kernel/*.c
+          ${REPO_ROOT_DIRECTORY}/libraries/libsrtp/crypto/math/*.c
+          ${REPO_ROOT_DIRECTORY}/libraries/libsrtp/crypto/replay/*.c
+          ${REPO_ROOT_DIRECTORY}/libraries/libsrtp/srtp/*.c )
 set(LIBSRTP_SOURCES
           ${LIBSRTP_GLOB_SOURCES}
-          "${CMAKE_CURRENT_LIST_DIR}/../libraries/libsrtp/crypto/cipher/aes.c"
-          "${CMAKE_CURRENT_LIST_DIR}/../libraries/libsrtp/crypto/cipher/aes_gcm_mbedtls.c"
-          "${CMAKE_CURRENT_LIST_DIR}/../libraries/libsrtp/crypto/cipher/aes_icm_mbedtls.c"
-          "${CMAKE_CURRENT_LIST_DIR}/../libraries/libsrtp/crypto/cipher/cipher.c"
-          "${CMAKE_CURRENT_LIST_DIR}/../libraries/libsrtp/crypto/cipher/cipher_test_cases.c"
-          "${CMAKE_CURRENT_LIST_DIR}/../libraries/libsrtp/crypto/cipher/null_cipher.c"
-          "${CMAKE_CURRENT_LIST_DIR}/../libraries/libsrtp/crypto/hash/auth_test_cases.c"
-          "${CMAKE_CURRENT_LIST_DIR}/../libraries/libsrtp/crypto/hash/auth.c"
-          "${CMAKE_CURRENT_LIST_DIR}/../libraries/libsrtp/crypto/hash/hmac.c"
-          "${CMAKE_CURRENT_LIST_DIR}/../libraries/libsrtp/crypto/hash/hmac_mbedtls.c"
-          "${CMAKE_CURRENT_LIST_DIR}/../libraries/libsrtp/crypto/hash/null_auth.c"
-          "${CMAKE_CURRENT_LIST_DIR}/../libraries/libsrtp/crypto/hash/sha1.c"
+          "${REPO_ROOT_DIRECTORY}/libraries/libsrtp/crypto/cipher/aes.c"
+          "${REPO_ROOT_DIRECTORY}/libraries/libsrtp/crypto/cipher/aes_gcm_mbedtls.c"
+          "${REPO_ROOT_DIRECTORY}/libraries/libsrtp/crypto/cipher/aes_icm_mbedtls.c"
+          "${REPO_ROOT_DIRECTORY}/libraries/libsrtp/crypto/cipher/cipher.c"
+          "${REPO_ROOT_DIRECTORY}/libraries/libsrtp/crypto/cipher/cipher_test_cases.c"
+          "${REPO_ROOT_DIRECTORY}/libraries/libsrtp/crypto/cipher/null_cipher.c"
+          "${REPO_ROOT_DIRECTORY}/libraries/libsrtp/crypto/hash/auth_test_cases.c"
+          "${REPO_ROOT_DIRECTORY}/libraries/libsrtp/crypto/hash/auth.c"
+          "${REPO_ROOT_DIRECTORY}/libraries/libsrtp/crypto/hash/hmac.c"
+          "${REPO_ROOT_DIRECTORY}/libraries/libsrtp/crypto/hash/hmac_mbedtls.c"
+          "${REPO_ROOT_DIRECTORY}/libraries/libsrtp/crypto/hash/null_auth.c"
+          "${REPO_ROOT_DIRECTORY}/libraries/libsrtp/crypto/hash/sha1.c"
           )
 
 # libsrtp library Public Include directories.
 set( LIBSRTP_INCLUDE_PUBLIC_DIRS
-     "${CMAKE_CURRENT_LIST_DIR}/../examples/libsrtp"
-     "${CMAKE_CURRENT_LIST_DIR}/../libraries/libsrtp/include"
-     "${CMAKE_CURRENT_LIST_DIR}/../libraries/libsrtp/crypto/include" )
+     "${REPO_ROOT_DIRECTORY}/examples/libsrtp"
+     "${REPO_ROOT_DIRECTORY}/libraries/libsrtp/include"
+     "${REPO_ROOT_DIRECTORY}/libraries/libsrtp/crypto/include" )
 
-add_library(
-     ${libsrtp} STATIC
-     ${LIBSRTP_SOURCES}
+add_library( ${libsrtp} )
+
+target_sources( ${libsrtp}
+    PRIVATE
+        ${LIBSRTP_SOURCES}
+    PUBLIC
+        ${LIBSRTP_INCLUDE_PUBLIC_DIRS}
+        ${inc_path}
+        ${REPO_ROOT_DIRECTORY}/libraries/ambpro2_sdk/component/os/freertos/${freertos}/Source/portable/GCC/ARM_CM33_NTZ/non_secure
 )
 
 list(
@@ -43,7 +50,6 @@ list(
 
 target_compile_definitions(${libsrtp} PRIVATE ${libsrtp_flags})
 
-include( ${CMAKE_CURRENT_LIST_DIR}/../project/realtek_amebapro2_webrtc_application/GCC-RELEASE/includepath.cmake )
 target_include_directories(
 	${libsrtp}
 	PUBLIC
@@ -55,5 +61,5 @@ target_include_directories(
 ### add linked library ###
 list(
     APPEND app_example_lib
-    libsrtp
+    ${libsrtp}
 )
