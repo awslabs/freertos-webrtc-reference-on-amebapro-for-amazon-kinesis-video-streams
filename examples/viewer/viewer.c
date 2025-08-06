4194a5a521f5e72c620cf81e2b75d95ce20be3e1
ActoryOu <ousc@amazon.com>
2025-08-06 09:31:27 +0000

    Create viewer build framework

--- examples/viewer/viewer.c
+++ examples/viewer/viewer.c
@@ -14,109 +14,319 @@
  * limitations under the License.
  */
 
-/* This file is not compiled now. The function here is prepared for future use as viewer side. */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "sys_api.h"      /* sys_backtrace_enable() */
+#include "sntp/sntp.h"    /* SNTP series APIs */
+#include "wifi_conf.h"    /* WiFi series APIs */
+#include "lwip_netconf.h" /* LwIP_GetIP() */
+#include "srtp.h"
+
+#include "demo_config.h"
+#include "app_common.h"
+#include "app_media_source.h"
+#include "logging.h"
+
 #define DEMO_MASTER_CLIENT_ID "ProduceMaster"
 #define DEMO_MASTER_CLIENT_ID_LENGTH ( 13 )
 
-static int32_t CreateSdpOffer( AppContext_t * pAppContext )
+AppContext_t appContext;
+AppMediaSourcesContext_t appMediaSourceContext;
+
+static void Master_Task( void * pParameter );
+
+static int32_t InitTransceiver( void * pMediaCtx,
+                                TransceiverTrackKind_t trackKind,
+                                Transceiver_t * pTranceiver );
+static int32_t OnMediaSinkHook( void * pCustom,
+                                MediaFrame_t * pFrame );
+static int32_t InitializeAppMediaSource( AppContext_t * pAppContext,
+                                         AppMediaSourcesContext_t * pAppMediaSourceContext );
+
+static int32_t InitTransceiver( void * pMediaCtx,
+                                TransceiverTrackKind_t trackKind,
+                                Transceiver_t * pTranceiver )
 {
     int32_t ret = 0;
-    uint8_t skipProcess = 0;
-    SignalingControllerResult_t signalingControllerReturn;
-    PeerConnectionResult_t peerConnectionResult;
-    PeerConnectionBufferSessionDescription_t bufferSessionDescription;
-    size_t sdpOfferMessageLength = 0;
-    AppSession_t * pAppSession = NULL;
-    SignalingControllerEventMessage_t eventMessage = {
-        .event = SIGNALING_CONTROLLER_EVENT_SEND_WSS_MESSAGE,
-        .onCompleteCallback = NULL,
-        .pOnCompleteCallbackContext = NULL,
-    };
+    AppMediaSourcesContext_t * pMediaSourceContext = ( AppMediaSourcesContext_t * )pMediaCtx;
 
-    if( skipProcess == 0 )
+    if( ( pMediaCtx == NULL ) || ( pTranceiver == NULL ) )
     {
-        pAppSession = GetCreatePeerConnectionSession( pAppContext, DEMO_MASTER_CLIENT_ID, DEMO_MASTER_CLIENT_ID_LENGTH, 1U );
-        if( pAppSession == NULL )
-        {
-            LogWarn( ( "No available peer connection session for remote client ID(%u): %.*s",
-                       DEMO_MASTER_CLIENT_ID_LENGTH,
-                       ( int ) DEMO_MASTER_CLIENT_ID_LENGTH,
-                       DEMO_MASTER_CLIENT_ID ) );
-            skipProcess = 1;
-        }
+        LogError( ( "Invalid input, pMediaCtx: %p, pTranceiver: %p", pMediaCtx, pTranceiver ) );
+        ret = -1;
+    }
+    else if( ( trackKind != TRANSCEIVER_TRACK_KIND_VIDEO ) &&
+             ( trackKind != TRANSCEIVER_TRACK_KIND_AUDIO ) )
+    {
+        LogError( ( "Invalid track kind: %d", trackKind ) );
+        ret = -2;
+    }
+    else
+    {
+        /* Empty else marker. */
     }
 
-    if( skipProcess == 0 )
+    if( ret == 0 )
     {
-        memset( &bufferSessionDescription, 0, sizeof( PeerConnectionBufferSessionDescription_t ) );
-        bufferSessionDescription.pSdpBuffer = pAppContext->sdpBuffer;
-        bufferSessionDescription.sdpBufferLength = PEER_CONNECTION_SDP_DESCRIPTION_BUFFER_MAX_LENGTH;
-        peerConnectionResult = PeerConnection_SetLocalDescription( &pAppSession->peerConnectionSession,
-                                                                   &bufferSessionDescription );
-        if( peerConnectionResult != PEER_CONNECTION_RESULT_OK )
+        switch( trackKind )
         {
-            LogWarn( ( "PeerConnection_SetLocalDescription fail, result: %d.", peerConnectionResult ) );
+            case TRANSCEIVER_TRACK_KIND_VIDEO:
+                ret = AppMediaSource_InitVideoTransceiver( pMediaSourceContext,
+                                                           pTranceiver );
+                break;
+            case TRANSCEIVER_TRACK_KIND_AUDIO:
+                ret = AppMediaSource_InitAudioTransceiver( pMediaSourceContext,
+                                                           pTranceiver );
+                break;
+            default:
+                LogError( ( "Invalid track kind: %d", trackKind ) );
+                ret = -3;
+                break;
         }
-    }
-
-    if( skipProcess == 0 )
-    {
-        pAppContext->sdpConstructedBufferLength = PEER_CONNECTION_SDP_DESCRIPTION_BUFFER_MAX_LENGTH;
-        peerConnectionResult = PeerConnection_CreateOffer( &pAppSession->peerConnectionSession,
-                                                           &bufferSessionDescription,
-                                                           pAppContext->sdpConstructedBuffer,
-                                                           &pAppContext->sdpConstructedBufferLength );
-        if( peerConnectionResult != PEER_CONNECTION_RESULT_OK )
-        {
-            LogWarn( ( "PeerConnection_CreateOffer fail, result: %d.", peerConnectionResult ) );
-            skipProcess = 1;
-        }
-    }
-
-    if( skipProcess == 0 )
-    {
-        /* Translate from SDP formal format into signaling event message by replacing newline with "\\n" or "\\r\\n". */
-        sdpOfferMessageLength = PEER_CONNECTION_SDP_DESCRIPTION_BUFFER_MAX_LENGTH;
-        signalingControllerReturn = SignalingController_SerializeSdpContentNewline( pAppContext->sdpConstructedBuffer,
-                                                                                    pAppContext->sdpConstructedBufferLength,
-                                                                                    pAppContext->sdpBuffer,
-                                                                                    &sdpOfferMessageLength );
-        if( signalingControllerReturn != SIGNALING_CONTROLLER_RESULT_OK )
-        {
-            LogError( ( "Fail to deserialize SDP offer newline, result: %d, constructed buffer(%u): %.*s",
-                        signalingControllerReturn,
-                        pAppContext->sdpConstructedBufferLength,
-                        ( int ) pAppContext->sdpConstructedBufferLength,
-                        pAppContext->sdpConstructedBuffer ) );
-            skipProcess = 1;
-        }
-    }
-
-    if( skipProcess == 0 )
-    {
-        eventMessage.eventContent.correlationIdLength = 0U;
-        memset( eventMessage.eventContent.correlationId, 0, SECRET_ACCESS_KEY_MAX_LEN );
-        eventMessage.eventContent.messageType = SIGNALING_TYPE_MESSAGE_SDP_OFFER;
-        eventMessage.eventContent.pDecodeMessage = pAppContext->sdpBuffer;
-        eventMessage.eventContent.decodeMessageLength = sdpOfferMessageLength;
-        memcpy( eventMessage.eventContent.remoteClientId, pEvent->pRemoteClientId, pEvent->remoteClientIdLength );
-        eventMessage.eventContent.remoteClientIdLength = pEvent->remoteClientIdLength;
-
-        signalingControllerReturn = SignalingController_SendMessage( &pAppContext->ignalingControllerContext, &eventMessage );
-        if( signalingControllerReturn != SIGNALING_CONTROLLER_RESULT_OK )
-        {
-            skipProcess = 1;
-            LogError( ( "Send signaling message fail, result: %d", signalingControllerReturn ) );
-        }
-    }
-
-    if( skipProcess == 0 )
-    {
-        LogInfo( ( "Created SDP offer(%u): %.*s",
-                   sdpOfferMessageLength,
-                   ( int ) sdpOfferMessageLength,
-                   pAppContext->sdpBuffer ) );
     }
 
     return ret;
 }
+
+static int32_t OnMediaSinkHook( void * pCustom,
+                                MediaFrame_t * pFrame )
+{
+    int32_t ret = 0;
+    AppContext_t * pAppContext = ( AppContext_t * ) pCustom;
+    PeerConnectionResult_t peerConnectionResult;
+    Transceiver_t * pTransceiver = NULL;
+    PeerConnectionFrame_t peerConnectionFrame;
+    int i;
+
+    if( ( pAppContext == NULL ) || ( pFrame == NULL ) )
+    {
+        LogError( ( "Invalid input, pCustom: %p, pFrame: %p", pCustom, pFrame ) );
+        ret = -1;
+    }
+
+    if( ret == 0 )
+    {
+        peerConnectionFrame.version = PEER_CONNECTION_FRAME_CURRENT_VERSION;
+        peerConnectionFrame.presentationUs = pFrame->timestampUs;
+        peerConnectionFrame.pData = pFrame->pData;
+        peerConnectionFrame.dataLength = pFrame->size;
+
+        for( i = 0; i < AWS_MAX_VIEWER_NUM; i++ )
+        {
+            if( pFrame->trackKind == TRANSCEIVER_TRACK_KIND_VIDEO )
+            {
+                pTransceiver = &pAppContext->appSessions[ i ].transceivers[ DEMO_TRANSCEIVER_MEDIA_INDEX_VIDEO ];
+            }
+            else if( pFrame->trackKind == TRANSCEIVER_TRACK_KIND_AUDIO )
+            {
+                pTransceiver = &pAppContext->appSessions[ i ].transceivers[ DEMO_TRANSCEIVER_MEDIA_INDEX_AUDIO ];
+            }
+            else
+            {
+                /* Unknown kind, skip that. */
+                LogWarn( ( "Unknown track kind: %d", pFrame->trackKind ) );
+                break;
+            }
+
+            if( pAppContext->appSessions[ i ].peerConnectionSession.state == PEER_CONNECTION_SESSION_STATE_CONNECTION_READY )
+            {
+                peerConnectionResult = PeerConnection_WriteFrame( &pAppContext->appSessions[ i ].peerConnectionSession,
+                                                                  pTransceiver,
+                                                                  &peerConnectionFrame );
+
+                if( peerConnectionResult != PEER_CONNECTION_RESULT_OK )
+                {
+                    LogError( ( "Fail to write %s frame, result: %d", ( pFrame->trackKind == TRANSCEIVER_TRACK_KIND_VIDEO ) ? "video" : "audio",
+                                peerConnectionResult ) );
+                    ret = -3;
+                }
+            }
+        }
+    }
+
+    return ret;
+}
+
+static int32_t InitializeAppMediaSource( AppContext_t * pAppContext,
+                                         AppMediaSourcesContext_t * pAppMediaSourceContext )
+{
+    int32_t ret = 0;
+
+    if( ( pAppContext == NULL ) ||
+        ( pAppMediaSourceContext == NULL ) )
+    {
+        LogError( ( "Invalid input, pAppContext: %p, pAppMediaSourceContext: %p", pAppContext, pAppMediaSourceContext ) );
+        ret = -1;
+    }
+
+    if( ret == 0 )
+    {
+        ret = AppMediaSource_Init( pAppMediaSourceContext,
+                                   OnMediaSinkHook,
+                                   pAppContext );
+    }
+
+    return ret;
+}
+
+static void Master_Task( void * pParameter )
+{
+    int32_t ret = 0;
+
+    ( void ) pParameter;
+
+    LogInfo( ( "Start Master_Task." ) );
+
+    ret = AppCommon_Init( &appContext, InitTransceiver, &appMediaSourceContext );
+
+    if( ret == 0 )
+    {
+        ret = InitializeAppMediaSource( &appContext, &appMediaSourceContext );
+    }
+
+    if( ret == 0 )
+    {
+        /* Launch application with current thread serving as Signaling Controller. */
+        ret = AppCommon_Start( &appContext );
+    }
+
+    for( ;; )
+    {
+        vTaskDelay( pdMS_TO_TICKS( 200 ) );
+    }
+}
+
+void app_example( void )
+{
+    int ret = 0;
+
+    if( ret == 0 )
+    {
+        #ifdef BUILD_INFO
+        LogInfo( ( "\r\nBuild Info: %s\r\n", BUILD_INFO ) );
+        #endif
+    }
+
+    if( ret == 0 )
+    {
+        if( xTaskCreate( Master_Task,
+                         ( ( const char * ) "MasterTask" ),
+                         20480,
+                         NULL,
+                         tskIDLE_PRIORITY + 4,
+                         NULL ) != pdPASS )
+        {
+            LogError( ( "xTaskCreate(Master_Task) failed" ) );
+            ret = -1;
+        }
+    }
+}
+
+// static int32_t CreateSdpOffer( AppContext_t * pAppContext )
+// {
+//     int32_t ret = 0;
+//     uint8_t skipProcess = 0;
+//     SignalingControllerResult_t signalingControllerReturn;
+//     PeerConnectionResult_t peerConnectionResult;
+//     PeerConnectionBufferSessionDescription_t bufferSessionDescription;
+//     size_t sdpOfferMessageLength = 0;
+//     AppSession_t * pAppSession = NULL;
+//     SignalingControllerEventMessage_t eventMessage = {
+//         .event = SIGNALING_CONTROLLER_EVENT_SEND_WSS_MESSAGE,
+//         .onCompleteCallback = NULL,
+//         .pOnCompleteCallbackContext = NULL,
+//     };
+
+//     if( skipProcess == 0 )
+//     {
+//         pAppSession = GetCreatePeerConnectionSession( pAppContext, DEMO_MASTER_CLIENT_ID, DEMO_MASTER_CLIENT_ID_LENGTH, 1U );
+//         if( pAppSession == NULL )
+//         {
+//             LogWarn( ( "No available peer connection session for remote client ID(%u): %.*s",
+//                        DEMO_MASTER_CLIENT_ID_LENGTH,
+//                        ( int ) DEMO_MASTER_CLIENT_ID_LENGTH,
+//                        DEMO_MASTER_CLIENT_ID ) );
+//             skipProcess = 1;
+//         }
+//     }
+
+//     if( skipProcess == 0 )
+//     {
+//         memset( &bufferSessionDescription, 0, sizeof( PeerConnectionBufferSessionDescription_t ) );
+//         bufferSessionDescription.pSdpBuffer = pAppContext->sdpBuffer;
+//         bufferSessionDescription.sdpBufferLength = PEER_CONNECTION_SDP_DESCRIPTION_BUFFER_MAX_LENGTH;
+//         peerConnectionResult = PeerConnection_SetLocalDescription( &pAppSession->peerConnectionSession,
+//                                                                    &bufferSessionDescription );
+//         if( peerConnectionResult != PEER_CONNECTION_RESULT_OK )
+//         {
+//             LogWarn( ( "PeerConnection_SetLocalDescription fail, result: %d.", peerConnectionResult ) );
+//         }
+//     }
+
+//     if( skipProcess == 0 )
+//     {
+//         pAppContext->sdpConstructedBufferLength = PEER_CONNECTION_SDP_DESCRIPTION_BUFFER_MAX_LENGTH;
+//         peerConnectionResult = PeerConnection_CreateOffer( &pAppSession->peerConnectionSession,
+//                                                            &bufferSessionDescription,
+//                                                            pAppContext->sdpConstructedBuffer,
+//                                                            &pAppContext->sdpConstructedBufferLength );
+//         if( peerConnectionResult != PEER_CONNECTION_RESULT_OK )
+//         {
+//             LogWarn( ( "PeerConnection_CreateOffer fail, result: %d.", peerConnectionResult ) );
+//             skipProcess = 1;
+//         }
+//     }
+
+//     if( skipProcess == 0 )
+//     {
+//         /* Translate from SDP formal format into signaling event message by replacing newline with "\\n" or "\\r\\n". */
+//         sdpOfferMessageLength = PEER_CONNECTION_SDP_DESCRIPTION_BUFFER_MAX_LENGTH;
+//         signalingControllerReturn = SignalingController_SerializeSdpContentNewline( pAppContext->sdpConstructedBuffer,
+//                                                                                     pAppContext->sdpConstructedBufferLength,
+//                                                                                     pAppContext->sdpBuffer,
+//                                                                                     &sdpOfferMessageLength );
+//         if( signalingControllerReturn != SIGNALING_CONTROLLER_RESULT_OK )
+//         {
+//             LogError( ( "Fail to deserialize SDP offer newline, result: %d, constructed buffer(%u): %.*s",
+//                         signalingControllerReturn,
+//                         pAppContext->sdpConstructedBufferLength,
+//                         ( int ) pAppContext->sdpConstructedBufferLength,
+//                         pAppContext->sdpConstructedBuffer ) );
+//             skipProcess = 1;
+//         }
+//     }
+
+//     if( skipProcess == 0 )
+//     {
+//         eventMessage.eventContent.correlationIdLength = 0U;
+//         memset( eventMessage.eventContent.correlationId, 0, SECRET_ACCESS_KEY_MAX_LEN );
+//         eventMessage.eventContent.messageType = SIGNALING_TYPE_MESSAGE_SDP_OFFER;
+//         eventMessage.eventContent.pDecodeMessage = pAppContext->sdpBuffer;
+//         eventMessage.eventContent.decodeMessageLength = sdpOfferMessageLength;
+//         memcpy( eventMessage.eventContent.remoteClientId, pEvent->pRemoteClientId, pEvent->remoteClientIdLength );
+//         eventMessage.eventContent.remoteClientIdLength = pEvent->remoteClientIdLength;
+
+//         signalingControllerReturn = SignalingController_SendMessage( &pAppContext->ignalingControllerContext, &eventMessage );
+//         if( signalingControllerReturn != SIGNALING_CONTROLLER_RESULT_OK )
+//         {
+//             skipProcess = 1;
+//             LogError( ( "Send signaling message fail, result: %d", signalingControllerReturn ) );
+//         }
+//     }
+
+//     if( skipProcess == 0 )
+//     {
+//         LogInfo( ( "Created SDP offer(%u): %.*s",
+//                    sdpOfferMessageLength,
+//                    ( int ) sdpOfferMessageLength,
+//                    pAppContext->sdpBuffer ) );
+//     }
+
+//     return ret;
+// }
---
+++ project/webrtc_demo.cmake
@@ -0,0 +1,192 @@
+cmake_minimum_required(VERSION 3.6.3)
+
+set( REPO_ROOT_DIRECTORY ${repo_root} )
+
+option(ENABLE_STREAMING_LOOPBACK "Loopback the received frames to the remote peer" OFF)
+
+# Option to control linking with usrsctp
+option( BUILD_USRSCTP_LIBRARY "Enable linking with usrsctp" ON )
+
+# Option to enable metric logging
+option( METRIC_PRINT_ENABLED "Enable Metric print logging" OFF )
+
+# Option to choose the target type, either master or viewer application
+option( BUILD_VIEWER_APPLICATION "Build Viewer Application" OFF )
+
+if( BUILD_VIEWER_APPLICATION )
+	set( WEBRTC_APPLICATION_DEMO_TYPE "viewer" CACHE STRING "Build WebRTC Viewer Application" )
+else()
+	set( WEBRTC_APPLICATION_DEMO_TYPE "master" CACHE STRING "Build WebRTC Master Application" )
+endif()
+
+file( GLOB WEBRTC_APPLICATION_SOURCE_FILES
+    "${REPO_ROOT_DIRECTORY}/examples/peer_connection/*.c"
+    "${REPO_ROOT_DIRECTORY}/examples/peer_connection/peer_connection_codec_helper/*.c"
+    "${REPO_ROOT_DIRECTORY}/examples/signaling_controller/*.c"
+    "${REPO_ROOT_DIRECTORY}/examples/network_transport/*.c"
+    "${REPO_ROOT_DIRECTORY}/examples/network_transport/tcp_sockets_wrapper/ports/lwip/*.c"
+    "${REPO_ROOT_DIRECTORY}/examples/network_transport/udp_sockets_wrapper/ports/lwip/*.c"
+    "${REPO_ROOT_DIRECTORY}/examples/networking/corehttp_helper/*.c"
+    "${REPO_ROOT_DIRECTORY}/examples/networking/networking_utils/*.c"
+    "${REPO_ROOT_DIRECTORY}/examples/networking/wslay_helper/*.c"
+    "${REPO_ROOT_DIRECTORY}/examples/message_queue/*.c"
+    "${REPO_ROOT_DIRECTORY}/examples/base64/mbedtls/*.c"
+    "${REPO_ROOT_DIRECTORY}/examples/sdp_controller/*.c"
+    "${REPO_ROOT_DIRECTORY}/examples/string_utils/*.c"
+    "${REPO_ROOT_DIRECTORY}/examples/ice_controller/*.c"
+    "${REPO_ROOT_DIRECTORY}/examples/timer_controller/*.c"
+    "${REPO_ROOT_DIRECTORY}/examples/app_media_source/*.c"
+    "${REPO_ROOT_DIRECTORY}/examples/app_media_source/port/ameba_pro2/*.c" )
+
+set( WEBRTC_APPLICATION_INCLUDE_DIRS
+    "${REPO_ROOT_DIRECTORY}/examples/peer_connection"
+    "${REPO_ROOT_DIRECTORY}/examples/peer_connection/peer_connection_codec_helper"
+    "${REPO_ROOT_DIRECTORY}/examples/peer_connection/peer_connection_codec_helper/include"
+    "${REPO_ROOT_DIRECTORY}/examples/signaling_controller"
+    "${REPO_ROOT_DIRECTORY}/examples/network_transport"
+    "${REPO_ROOT_DIRECTORY}/examples/network_transport/tcp_sockets_wrapper/include"
+    "${REPO_ROOT_DIRECTORY}/examples/network_transport/udp_sockets_wrapper/include"
+    "${REPO_ROOT_DIRECTORY}/examples/networking"
+    "${REPO_ROOT_DIRECTORY}/examples/networking/corehttp_helper"
+    "${REPO_ROOT_DIRECTORY}/examples/networking/wslay_helper"
+    "${REPO_ROOT_DIRECTORY}/examples/networking/networking_utils"
+    "${REPO_ROOT_DIRECTORY}/examples/logging"
+    "${REPO_ROOT_DIRECTORY}/examples/message_queue"
+    "${REPO_ROOT_DIRECTORY}/examples/base64"
+    "${REPO_ROOT_DIRECTORY}/examples/sdp_controller"
+    "${REPO_ROOT_DIRECTORY}/examples/string_utils"
+    "${REPO_ROOT_DIRECTORY}/examples/ice_controller"
+    "${REPO_ROOT_DIRECTORY}/examples/timer_controller"
+    "${REPO_ROOT_DIRECTORY}/examples/app_media_source"
+    "${REPO_ROOT_DIRECTORY}/examples/app_media_source/port/ameba_pro2"
+    "${REPO_ROOT_DIRECTORY}/examples/demo_config" )
+
+if( BUILD_USRSCTP_LIBRARY )
+    file( GLOB USRSCTP_SRC_FILES "${REPO_ROOT_DIRECTORY}/examples/libusrsctp/*.c" )
+    list( APPEND WEBRTC_APPLICATION_SOURCE_FILES ${USRSCTP_SRC_FILES} )
+    list( APPEND WEBRTC_APPLICATION_INCLUDE_DIRS "${REPO_ROOT_DIRECTORY}/examples/libusrsctp" )
+endif()
+
+if( ${WEBRTC_APPLICATION_DEMO_TYPE} STREQUAL "master" )
+    message( STATUS "Building Master Application" )
+    file( GLOB WEBRTC_APPLICATION_MASTER_SOURCE_FILES
+          "${REPO_ROOT_DIRECTORY}/examples/master/*.c"
+          "${REPO_ROOT_DIRECTORY}/examples/app_common/*.c" )
+    list( APPEND WEBRTC_APPLICATION_SOURCE_FILES
+          ${WEBRTC_APPLICATION_MASTER_SOURCE_FILES} )
+    list( APPEND WEBRTC_APPLICATION_INCLUDE_DIRS
+          "${REPO_ROOT_DIRECTORY}/examples/master"
+          "${REPO_ROOT_DIRECTORY}/examples/app_common" )
+elseif( ${WEBRTC_APPLICATION_DEMO_TYPE} STREQUAL "viewer" )
+    message( STATUS "Building Viewer Application" )
+    file( GLOB WEBRTC_APPLICATION_VIEWER_SOURCE_FILES
+          "${REPO_ROOT_DIRECTORY}/examples/viewer/*.c"
+          "${REPO_ROOT_DIRECTORY}/examples/app_common/*.c" )
+    list( APPEND WEBRTC_APPLICATION_SOURCE_FILES
+          ${WEBRTC_APPLICATION_VIEWER_SOURCE_FILES} )
+    list( APPEND WEBRTC_APPLICATION_INCLUDE_DIRS
+          "${REPO_ROOT_DIRECTORY}/examples/viewer"
+          "${REPO_ROOT_DIRECTORY}/examples/app_common" )
+endif()
+ 
+if( METRIC_PRINT_ENABLED )
+     file( GLOB METRIC_SRC_FILES "${REPO_ROOT_DIRECTORY}/examples/metric/*.c" )
+     list( APPEND WEBRTC_APPLICATION_SOURCE_FILES ${METRIC_SRC_FILES} )
+     list( APPEND WEBRTC_APPLICATION_INCLUDE_DIRS "${REPO_ROOT_DIRECTORY}/examples/metric" )
+endif()
+ 
+# Include dependencies
+# Include coreHTTP
+include( ${REPO_ROOT_DIRECTORY}/CMake/coreHTTP.cmake )
+
+# Include sigV4
+include( ${REPO_ROOT_DIRECTORY}/CMake/sigV4.cmake )
+
+## Include coreJSON
+include( ${REPO_ROOT_DIRECTORY}/CMake/coreJSON.cmake )
+
+## Include Signaling
+include( ${REPO_ROOT_DIRECTORY}/CMake/signaling.cmake )
+
+# Suppress warnings for some Libraries
+file(GLOB_RECURSE WARNING_SUPPRESSED_SOURCES
+    "${REPO_ROOT_DIRECTORY}/libraries/ambpro2_sdk/*.c"
+    "${REPO_ROOT_DIRECTORY}/libraries/usrsctp/*.c"
+)
+
+set_source_files_properties(
+    ${WARNING_SUPPRESSED_SOURCES}
+    PROPERTIES
+    COMPILE_FLAGS "-w"
+)
+
+# Include wslay
+include( ${REPO_ROOT_DIRECTORY}/CMake/wslay.cmake )
+
+# Include SDP
+include( ${REPO_ROOT_DIRECTORY}/CMake/sdp.cmake )
+
+# Include STUN
+include( ${REPO_ROOT_DIRECTORY}/CMake/stun.cmake )
+
+# Include RTP
+include( ${REPO_ROOT_DIRECTORY}/CMake/rtp.cmake )
+
+# Include RTCP
+include( ${REPO_ROOT_DIRECTORY}/CMake/rtcp.cmake )
+
+# Include ICE
+include( ${REPO_ROOT_DIRECTORY}/CMake/ice.cmake )
+
+# Include libsrtp
+include( ${REPO_ROOT_DIRECTORY}/CMake/libsrtp.cmake )
+
+set( webrtc_demo_src
+     ${WEBRTC_APPLICATION_SOURCE_FILES} )
+
+set( webrtc_demo_include
+     ${WEBRTC_APPLICATION_INCLUDE_DIRS} )
+
+if(BUILD_USRSCTP_LIBRARY)
+     # Include DCEP
+     include( ${REPO_ROOT_DIRECTORY}/CMake/dcep.cmake )
+     # Include usrsctp
+     include( ${REPO_ROOT_DIRECTORY}/CMake/usrsctp.cmake )
+
+     list(
+          APPEND app_flags
+          ENABLE_SCTP_DATA_CHANNEL=1
+     )
+else()
+     list(
+          APPEND app_flags
+          ENABLE_SCTP_DATA_CHANNEL=0
+     )
+endif()
+
+if(METRIC_PRINT_ENABLED)
+     list(
+          APPEND app_flags
+          METRIC_PRINT_ENABLED=1
+     )
+else()
+     list(
+          APPEND app_flags
+          METRIC_PRINT_ENABLED=0
+     )
+endif()
+
+# Set more strict rules to application code only
+set_source_files_properties(
+     ${WEBRTC_APPLICATION_SOURCE_FILES}
+     ${WEBRTC_APPLICATION_INCLUDE_DIRS}
+     PROPERTIES
+     COMPILE_FLAGS "-Werror"
+)
+
+if( ENABLE_STREAMING_LOOPBACK )
+     list(
+          APPEND app_flags
+          ENABLE_STREAMING_LOOPBACK
+     )
+endif()
